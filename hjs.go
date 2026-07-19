bdfe5859e2ed66079a07b7fe09d408180cf209fd
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-07-19 06:31:41 +0100

    refactor: use latest xjs API

--- hjs.go
+++ hjs.go
@@ -5,11 +5,13 @@ import (
 	"github.com/xjslang/xjs"
 	"github.com/xjslang/xjs/ast"
 	"github.com/xjslang/xjs/js"
+	"github.com/xjslang/xjs/jsextended"
 	"github.com/xjslang/xjs/printer"
 )
 
 func Parse(input []byte) (*js.Program, error) {
 	p := xjs.PluginBuilder().
+		Install(jsextended.Plugin).
 		Install(html.Plugin).
 		Build(input)
 	return js.ParseProgram(p)
@@ -17,6 +19,7 @@ func Parse(input []byte) (*js.Program, error) {
 
 func Compile(result ast.Node) (string, error) {
 	pr := xjs.PrinterBuilder().
+		UsePrinter(jsextended.Printer).
 		UsePrinter(html.Compiler).
 		Build(printer.Compact())
 	pr.Print(result)
@@ -25,6 +28,7 @@ func Compile(result ast.Node) (string, error) {
 
 func Format(result ast.Node, opts ...printer.Option) (string, error) {
 	pr := xjs.PrinterBuilder().
+		UsePrinter(jsextended.Printer).
 		UsePrinter(html.Formatter).
 		Build(opts...)
 	pr.Print(result)
