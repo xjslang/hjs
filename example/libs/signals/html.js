65615099587af7c3f8dffb1c9861997d9e054a4e
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2026-07-22 00:01:46 +0100

    refactor: change domdiff signature

--- example/libs/signals/html.js
+++ example/libs/signals/html.js
@@ -15,41 +15,42 @@ export function Render(fn) {
   return node;
 }
 
-export function domdiff() {
-  const savedEffects = Object.create(null);
-
-  return function (parentNode, rows, insert) {
-    const prevEffects = currentEffects;
-    try {
-      const ids = {};
-      const rowIdSet = new Set(rows.map((r) => `${r.id}`));
-      for (const child of parentNode.children) {
-        const id = child.dataset.id;
-        if (id === undefined) {
-          throw new Error("All children must have dataset.id");
-        }
-        if (!rowIdSet.has(id) && savedEffects[id]) {
-          // cleaning effects group
-          savedEffects[id].clean();
-          delete savedEffects[id];
-        }
-        ids[id] = child;
+const effectsMap = new WeakMap();
+export function domdiff(parentNode, rows, insert) {
+  if (!effectsMap.has(parentNode)) {
+    effectsMap.set(parentNode, Object.create(null));
+  }
+  const savedEffects = effectsMap.get(parentNode);
+  const prevEffects = currentEffects;
+  try {
+    const ids = {};
+    const rowIdSet = new Set(rows.map((r) => `${r.id}`));
+    for (const child of parentNode.children) {
+      const id = child.dataset.id;
+      if (id === undefined) {
+        throw new Error("All children must have dataset.id");
       }
-      let nodes = [];
-      for (const row of rows) {
-        if (row.id === undefined || row.id === null) {
-          throw new Error("All rows must have an id property");
-        }
-        if (!savedEffects[row.id]) {
-          // creates an effects group per row
-          savedEffects[row.id] = ctx.effects();
-        }
-        currentEffects = savedEffects[row.id];
-        nodes.push(ids[`${row.id}`] || insert(row));
+      if (!rowIdSet.has(id) && savedEffects[id]) {
+        // cleaning effects group
+        savedEffects[id].clean();
+        delete savedEffects[id];
       }
-      udomdiff(parentNode, [...parentNode.children], nodes, (n) => n);
-    } finally {
-      currentEffects = prevEffects;
+      ids[id] = child;
     }
-  };
+    let nodes = [];
+    for (const row of rows) {
+      if (row.id === undefined || row.id === null) {
+        throw new Error("All rows must have an id property");
+      }
+      if (!savedEffects[row.id]) {
+        // creates an effects group per row
+        savedEffects[row.id] = ctx.effects();
+      }
+      currentEffects = savedEffects[row.id];
+      nodes.push(ids[`${row.id}`] || insert(row));
+    }
+    udomdiff(parentNode, [...parentNode.children], nodes, (n) => n);
+  } finally {
+    currentEffects = prevEffects;
+  }
 }
